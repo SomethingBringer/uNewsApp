4170f8c52707365390652f938c2727ffb2a36430
konovalovkirill <konovalov.k.a@yandex.ru>
2020-11-29 17:38:54 +0300

    Add UI for Stock Index Screen

---
+++ app/src/main/java/com/example/android/unewsapp/ui/fragments/index/StockIndexFragment.kt
@@ -0,0 +1,11 @@
+package com.example.android.unewsapp.ui.fragments.index
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.unewsapp.R
+
+class StockIndexFragment : Fragment(R.layout.stock_index_fragment) {
+    private val viewModel: StockIndexViewModel by viewModels()
+
+
+}
---
+++ app/src/main/java/com/example/android/unewsapp/ui/fragments/index/StockIndexViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android.unewsapp.ui.fragments.index
+
+import androidx.lifecycle.ViewModel
+
+class StockIndexViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
